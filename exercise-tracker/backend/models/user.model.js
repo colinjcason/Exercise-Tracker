beac2066cc74d32dad0a5291d7bc07676f056611
Colin Cason <colinjcason@gmail.com>
2020-09-05 16:36:50 -0700

    created server file, models, routes

---
+++ exercise-tracker/backend/models/user.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        // unique: true,
+        trim: true,
+        minlength: 3,
+        sparse: true
+    }
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
